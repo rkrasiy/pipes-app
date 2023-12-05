90483587116c2993018bb504385a094a0d62e1ba
ruslan.krasiy <ruslan.krasiy@alten.es>
2023-12-05 16:47:48 +0100

    Install primeNg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    ButtonModule,
+    SharedModule,
+
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MenuModule } from 'primeng/menu';
+
+@NgModule({
+  exports: [
+    MenuModule
+  ]
+})
+export class PrimeNgModule { }
